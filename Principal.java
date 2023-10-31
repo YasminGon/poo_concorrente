a3504402544e1a4a93f944938fc5fef8dfdd0675
YasminGon <yasmingon08@hotmail.com>
2023-10-31 14:05:57 +0000

    salvando

--- Principal.java
+++ Principal.java
@@ -2,6 +2,8 @@ public class Principal {
     public static void main(String[] args) {
         Processo proc1 = new Processo("Yasmin",17);
         Processo proc2 = new Processo("Joao Vitor",21);
+        proc1.start();
+        proc2.start();
 
         //objestos processo 1 e  processo 2
         //start
