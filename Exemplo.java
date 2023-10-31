74f68e08cbc1b878d443811972802664ddfa96c6
YasminGon <yasmingon08@hotmail.com>
2023-10-31 13:48:22 +0000

    salvando

---
+++ Exemplo.java
@@ -0,0 +1,14 @@
+public class Exemplo extends Thread{
+
+    public void run(){
+
+    }
+}
+
+//vamos criar uma classe Processo header de thread 
+//classe vai ter dois atributos 
+//String nome e o numeroInteracoes
+//metodo construtor com esses dois atributos acima
+//método run, no corpo dele estrutura de repetição
+//10 interaçãoes
+//nome e as interações 
---
+++ Principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+    public static void main(String[] args) {
+        Processo proc1 = new Processo("Yasmin",17);
+
+        //objestos processo 1 e  processo 2
+        //start
+    }
+}
